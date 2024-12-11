ca3cad21168752cad6b2c378966003e9725b6c41
aoi-shiraishi <aoi.shiraishi.biz@gmail.com>
2024-12-11 20:46:06 +0900

    RabbitMQContainerの作り方変更

--- src/test/kotlin/com/rabbitmqlistenertestsample/TestcontainersConfiguration.kt
+++ src/test/kotlin/com/rabbitmqlistenertestsample/TestcontainersConfiguration.kt
@@ -2,26 +2,18 @@ package com.rabbitmqlistenertestsample
 
 import org.springframework.amqp.core.Queue
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.RabbitMQContainer
+import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-//    @Bean
-//    @ServiceConnection
-//    fun rabbitContainer(): RabbitMQContainer {
-//        return RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"))
-//    }
-
-    // TODO: 上のコメントアウトだと動かない現象の解決方法を調査する
     @Bean
-    fun rabbitMQContainer(): RabbitMQContainer {
-        val container = RabbitMQContainer("rabbitmq:3.8-management-alpine")
-        container.start()
-        System.setProperty("spring.rabbitmq.host", container.host)
-        System.setProperty("spring.rabbitmq.port", container.getMappedPort(5672).toString())
-        return container
+    @ServiceConnection
+    fun rabbitContainer(): RabbitMQContainer {
+        return RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"))
     }
 
     @Bean
